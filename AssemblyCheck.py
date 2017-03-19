62d1504411db06edfbbb9ec8df4399f76c772cd0
Jasper de Meijer <Jasperdemeijer@gmail.com>
2017-03-19 11:42:42 +0100

    made SeqIO part more generically

--- AssemblyCheck.py
+++ AssemblyCheck.py
@@ -3,20 +3,24 @@ import sys
 import numpy
 
 def main():
-    scafLength = scaffoldLength()
-    nFiftyScore = nFifty(scafLength)
+    records = scaffoldLength()
+    nFiftyScore, nFiftyCount = nFifty(records)
+    print(nFiftyScore, nFiftyCount)
 
 
 def scaffoldLength():
     scafLength = []
+    records =[]
     for record in SeqIO.parse("scaffolds.fasta", "fasta"):
-
+        records.append(record)
         scafLength.append(len(record.seq))
+    return records
 
+def nFifty(records):
+    scafLength = []
+    for x in records:
+        scafLength.append(len(x.seq))
 
-    return scafLength
-
-def nFifty(scafLength):
     scafLength.sort(reverse=True)
     sumScaf = 0
     for x in scafLength:
@@ -26,14 +30,37 @@ def nFifty(scafLength):
 
     nSum = 0
     nFiftyCheck = False
-
+    scaffCounter = 0
     for x in scafLength:
+        scaffCounter += 1
         nSum += x
         if nSum >= halfSumScaf:
             nFifty = x
+            nFiftyCount = scaffCounter
             break
 
-    return nFifty
+    return nFifty, nFiftyCount
+
+# def gapStats(records):
+#
+#     for y in records:
+#         nCheck = False
+#         gapCount = 0
+#         gapSize = 0
+#         for x in y.seq:
+#             if x == "n":
+#                 gapSize += 1
+#                 while(nCheck == False):
+#                     gapCount += 1
+#                     nCheck = True
+#             else:
+#                 nCheck = False
+#
+#
+#         gapCountList = []
+#         averageGapSizeList = []
+
+
 
 
 
